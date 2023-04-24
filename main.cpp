ac6b65af9e0c8febb0a9b0749f5f5abce58ee392
Антон Брыляков <yoll@rambler.ru>
2023-04-23 21:25:53 -0700

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#ifdef _WIN32
+#include "windows.h"
+#endif
+#include "LogCommand.h"
+#include "ConsoleLogCommand.h"
+#include "FileLogCommand.h"
+
+void print(LogCommand& cmd, const std::string& message)
+{
+	cmd.print(message);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+#ifdef _WIN32
+	SetConsoleCP(1251);
+#endif
+
+	{
+		auto cmd = ConsoleLogCommand();
+		print(cmd, "Сообщение в консоль");
+	}
+
+	{
+		auto cmd = FileLogCommand("log.txt");
+		print(cmd, "Сообщение в файл");
+	}
+
+	return 0;
+}
